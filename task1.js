c6968f3bff98c50779f66156374ccd1ccd4f9d85
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-26 15:11:35 +0600

    task 2 done

--- task1.js
+++ task1.js
@@ -1,4 +1,14 @@
-var money = 1000;
-var expances = 700;
-var comeBack = money - expances;
-console.log(comeBack);
+// var money = 1000;
+// var expances = 700;
+// var comeBack = money - expances;
+// console.log(comeBack);
+
+// TASK 2
+var math = 65;
+var physics = 75;
+var Bangla = 60;
+var English = 66.5;
+var chemistry = 80.23;
+var total = math + physics + Bangla + English + chemistry;
+var average = total / 5;
+console.log(average);
