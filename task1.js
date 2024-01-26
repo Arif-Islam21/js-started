658aef13ad1bdfc43a76f5052c3ceda736471a3d
arifIslam12 <134288522+Arif-Islam21@users.noreply.github.com>
2024-01-26 15:13:43 +0600

    task 3 done

--- task1.js
+++ task1.js
@@ -4,11 +4,16 @@
 // console.log(comeBack);
 
 // TASK 2
-var math = 65;
-var physics = 75;
-var Bangla = 60;
-var English = 66.5;
-var chemistry = 80.23;
-var total = math + physics + Bangla + English + chemistry;
-var average = total / 5;
-console.log(average);
+// var math = 65;
+// var physics = 75;
+// var Bangla = 60;
+// var English = 66.5;
+// var chemistry = 80.23;
+// var total = math + physics + Bangla + English + chemistry;
+// var average = total / 5;
+// console.log(average);
+
+// TASK 3
+var number = 119;
+var remainder = number % 5;
+console.log(remainder);
